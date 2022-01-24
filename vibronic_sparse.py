a79b519f35928ad30ad0f09e040a216dbe993aa1
pnroy <pnroy@uwaterloo.ca>
2022-01-24 15:33:13 -0500

    Update vibronic_sparse.py

--- vibronic_sparse.py
+++ vibronic_sparse.py
@@ -99,7 +99,7 @@ def main(model, plotting=False):
                 u[(((a-1)*n1+i1)*n2+i2)]=param_times_grid2[i2]*v[((a*n1+i1)*n2+i2)]
         return u
  # basis sizes (store in dictionary for easy passing to functions)
-    n1, n2, na = 40, 40, 2
+    n1, n2, na = 20, 20, 2
     basis = {'n1': n1, 'n2': n2, 'a': na}
 
     # total size of product basis
@@ -133,7 +133,7 @@ def main(model, plotting=False):
     param_times_grid2=grid2.copy()
 
     if model=='Displaced':
-        param_times_grid2=displaced['lambda']*grid2
+        param_times_grid2=displaced['gamma'][system_index]*grid2
     if model=='Jahn_Teller':
         param_times_grid2=jahn_teller['lambda'][system_index]*grid2
 
@@ -176,11 +176,13 @@ def main(model, plotting=False):
     #evals, evecs = eigsh(A_total, k=kmax,which = 'SA', maxiter=niter)
     evals, evecs = eigsh(H_total, k=kmax,which = 'SA')
 
-    delta_E=evals[1]-evals[0]
+    if model=='Displaced':
+        delta_E=evals[1]-evals[0]
+    if model=='Jahn_Teller':
+        delta_E=evals[2]-evals[0] # use next gap because Jahn_Teller is degenerate
+
 
     # choose temperatures between 0.1 and 10 time the characteristic Theta=delta_E/eV_per_K
-    Tmin=1.*delta_E/eV_per_K
-    Tmax=3.*delta_E/eV_per_K
     Tmin=1.
     Tmax=1000.
     nT=30 # number of temperature values
@@ -211,6 +213,34 @@ def main(model, plotting=False):
             Ei=(evals[i]-evals[0])/eV_per_K
             Probs[t,i]=np.exp(-Ei/T[t])/Z[t]
 
+
+    if model=='Displaced':
+        labels='D, gamma='+str(displaced['gamma'][system_index])
+    if model=='Jahn_Teller':
+        labels='JT, E='+str(jahn_teller['energy'][system_index])+' lambda='+str(jahn_teller['lambda'][system_index])
+    print (labels,'Theta=',delta_E/eV_per_K,' K')
+    
+    fig_EV, ax_EV = plt.subplots()
+    fig_E, ax_E = plt.subplots()
+    fig_Cv, ax_Cv = plt.subplots()
+    fig_S, ax_S = plt.subplots()
+    fig_A, ax_A = plt.subplots()
+    ax_EV.plot([i for i in range(kmax)],(evals-evals[0])/eV_per_K,label=labels)
+    ax_E.plot(T,E,label=labels)
+    ax_Cv.plot(T,Cv,label=labels)
+    ax_A.plot(T,A,label=labels)
+    ax_S.plot(T,S,label=labels)
+    ax_EV.set(title='E(n) vs n '+str(model),xlabel='n',ylabel='E(n)/kB (K)')
+    ax_E.set(title='<E> vs T '+str(model),xlabel='T (K)',ylabel='<E>/kB (K)')
+    ax_Cv.set(title='Cv vs T '+str(model),xlabel='T (K)',ylabel='Cv/kB ')
+    ax_A.set(title='A vs T '+str(model),xlabel='T (K)',ylabel='A/kB (K)')
+    ax_S.set(title='S vs T '+str(model),xlabel='T (K)',ylabel='S/kB')
+    fig_EV.savefig('Evsn_'+str(model)+str(system_index)+'.png')
+    fig_E.savefig('EvsT_'+str(model)+str(system_index)+'.png')
+    fig_Cv.savefig('CvvsT_'+str(model)+str(system_index)+'.png')
+    fig_A.savefig('AvsT_'+str(model)+str(system_index)+'.png')
+    fig_S.savefig('SvsT_'+str(model)+str(system_index)+'.png')  
+
     rho1=np.zeros((n1),float)
     rho2=np.zeros((n2),float)
     rho12=np.zeros((n1,n2),float)
@@ -218,18 +248,21 @@ def main(model, plotting=False):
     rhoa=np.zeros((na,na),float)
 
     # calculate distributions for each temperature
-    
-    for tindex in range(len(T)):
+
+    Tlist=[0.1, 1., 2., 5., 10.]
+    for tindex in Tlist:
     #output files
-        rho1_out=open('h1_T'+str(T[tindex])+'.dat','w')
-        rho2_out=open('h2_T'+str(T[tindex])+'.dat','w')
-        rho12_out=open('h12_T'+str(T[tindex])+'.dat','w')
-        w12_out=open('w12_T'+str(T[tindex])+'.dat','w')
-        rhoa_out=open('a_T'+str(T[tindex])+'.dat','w')
+        rho1_out=open('h1_T'+str(tindex)+'.dat','w')
+        rho2_out=open('h2_T'+str(tindex)+'.dat','w')
+        rho12_out=open('h12_T'+str(tindex)+'.dat','w')
+        w12_out=open('w12_T'+str(tindex)+'.dat','w')
+        rhoa_out=open('a_T'+str(tindex)+'.dat','w')
         
-        t=T[tindex]
+        t=tindex*delta_E/eV_per_K
+        Z=0.
         for i in range(kmax):
             Ei=(evals[i]-evals[0])/eV_per_K
+            Z+=np.exp(-Ei/t)
             for a in range(na):
                 for ap in range(na):
                     for i1 in range(n1):
@@ -247,12 +280,12 @@ def main(model, plotting=False):
                 for i2 in range(n2):
                     for a in range(na):
                         rho12[i1,i2]+=(evecs[(a*n1+i1)*n2+i2,i]**2)*np.exp(-Ei/t)
-        rho1=(1./Z[tindex])*rho1
-        rho2=(1./Z[tindex])*rho2
-        rho12=(1./Z[tindex])*rho12
-        rhoa=(1./Z[tindex])*rhoa
+        rho1=(1./Z)*rho1
+        rho2=(1./Z)*rho2
+        rho12=(1./Z)*rho12
+        rhoa=(1./Z)*rhoa
 
-        w12=-T[tindex]*eV_per_K*np.log(rho12)
+        w12=-t*eV_per_K*np.log(rho12)
 
     #grid1, T1    
     #convert to grid
@@ -268,11 +301,15 @@ def main(model, plotting=False):
             h2[i2]=rho2[i2]
         # multiply by gauss hermite weight
             h2[i2]*=np.exp(-grid2[i2]**2)/(np.sqrt(np.pi)*T2[0,i2]**2)
+            rho2_out.write(str(grid2[i2])+' '+str(h1[i1])+'\n')
+
+            
         for i1 in range(n1):
             for i2 in range(n2):
             # multiply by gauss hermite weight
                 rho12[i1,i2]*=((np.exp(-grid2[i2]**2)/(np.sqrt(np.pi)*T2[0,i2]**2))*(np.exp(-grid1[i1]**2)/(np.sqrt(np.pi)*T1[0,i1]**2)))
-    
+                rho12_out.write(str(grid1[i1])+' '+str(grid2[i2])+' '+str(rho12[i1,i2])+'\n')
+                w12_out.write(str(grid1[i1])+' '+str(grid2[i2])+' '+str(w12[i1,i2])+'\n')
         rho1_out.close()
         rho2_out.close()
         rho12_out.close()
